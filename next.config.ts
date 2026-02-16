4c4f3044fb1e22f9ace741c236f548f69ecdd33c
Hassan <hassanisavailable@gmail.com>
2026-02-16 21:53:05 +0500

    some formating shits

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compress: true,
   poweredByHeader: false,
-  
+
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
