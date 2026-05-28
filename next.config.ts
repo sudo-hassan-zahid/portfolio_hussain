4f4e4e6b62f41b028419aa67d9ce220be889837e
Hassan <hassanisavailable@gmail.com>
2026-05-28 18:46:05 +0500

    Document Supabase setup

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,13 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 
   reactStrictMode: true,
